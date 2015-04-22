88b6a21d65c3b9b2b0b761955fdd6000ba7d9ae0
minfawan <minfa.wang@gmail.com>
2015-04-22 10:54:16 -0700

    finish yelp query

--- yelp_api/README.md
+++ yelp_api/README.md
@@ -1,12 +1,8 @@
-# Yelp API v2 Python Code Sample
+# YelpQuery
 
 ## Overview
-This program demonstrates the capability of the Yelp API version 2.0
-by using the Search API to query for businesses by a search term and location,
-and the Business API to query additional information about the top result
-from the search query.
 
-Please refer to [API documentation](http://www.yelp.com/developers/documentation)
+Please refer to [Yelp API documentation](http://www.yelp.com/developers/documentation)
 for more details.
 
 
@@ -15,8 +11,24 @@ for more details.
 To install the dependencies, run:
 `pip install -r requirements.txt`.
 
-Run the code sample without specifying any arguments:
-`python sample.py`
+Run the code by specifying the optional arguments:
 
-Run the code sample by specifying the optional arguments:
-`python sample.py --term="bars" --location="San Francisco, CA"`
+`python yelp_query.py --term bars -ll 34.1 -118.1`
+
+Or run the code inside a program
+
+```
+yelp = YelpQuery()
+
+"""
+query_api(self, ll,
+          term = DEFAULT_TERM, location = DEFAULT_LOCATION,
+          limit = DEFAULT_SEARCH_LIMIT, radius = DEFAULT_RADIUS)
+
+An exception will raise if invalid search condition is entered
+"""
+try:
+	businesses = yelp.query_api(ll=["34.1", "-118.1"], term="bar")
+except:
+	businesses = []
+```
--- yelp_api/yelp_query.py
+++ yelp_api/yelp_query.py
@@ -42,119 +42,120 @@ TOKEN = 'L9ZArB2hH2aJI9tjN-uJSZWOK7qjjreB'
 TOKEN_SECRET = '06ELQLKHZ9LGkHO3cGR_3Y4C0rE'
 
 
-def request(host, path, url_params=None):
-    """Prepares OAuth authentication and sends the request to the API.
+class YelpQuery:
+    def request(self, host, path, url_params=None):
+        """Prepares OAuth authentication and sends the request to the API.
 
-    Args:
-        host (str): The domain host of the API.
-        path (str): The path of the API after the domain.
-        url_params (dict): An optional set of query parameters in the request.
+        Args:
+            host (str): The domain host of the API.
+            path (str): The path of the API after the domain.
+            url_params (dict): An optional set of query parameters in the request.
 
-    Returns:
-        dict: The JSON response from the request.
+        Returns:
+            dict: The JSON response from the request.
 
-    Raises:
-        urllib2.HTTPError: An error occurs from the HTTP request.
-    """
-    url_params = url_params or {}
-    url = 'http://{0}{1}?'.format(host, urllib.quote(path.encode('utf8')))
+        Raises:
+            urllib2.HTTPError: An error occurs from the HTTP request.
+        """
+        url_params = url_params or {}
+        url = 'http://{0}{1}?'.format(host, urllib.quote(path.encode('utf8')))
 
-    consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
-    oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
+        consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+        oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
 
-    oauth_request.update(
-        {
-            'oauth_nonce': oauth2.generate_nonce(),
-            'oauth_timestamp': oauth2.generate_timestamp(),
-            'oauth_token': TOKEN,
-            'oauth_consumer_key': CONSUMER_KEY
-        }
-    )
-    token = oauth2.Token(TOKEN, TOKEN_SECRET)
-    oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
-    signed_url = oauth_request.to_url()
-    
-    print u'Querying {0} ...'.format(url)
+        oauth_request.update(
+            {
+                'oauth_nonce': oauth2.generate_nonce(),
+                'oauth_timestamp': oauth2.generate_timestamp(),
+                'oauth_token': TOKEN,
+                'oauth_consumer_key': CONSUMER_KEY
+            }
+        )
+        token = oauth2.Token(TOKEN, TOKEN_SECRET)
+        oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+        signed_url = oauth_request.to_url()
+        
+        # print u'Querying {0} ...'.format(url)
 
-    conn = urllib2.urlopen(signed_url, None)
-    try:
-        response = json.loads(conn.read())
-    finally:
-        conn.close()
+        conn = urllib2.urlopen(signed_url, None)
+        try:
+            response = json.loads(conn.read())
+        finally:
+            conn.close()
 
-    return response
+        return response
 
-def search(term, location, ll, limit, radius):
-    """Query the Search API by a search term and location.
+    def search(self, term, location, ll, limit, radius):
+        """Query the Search API by a search term and location.
 
-    Args:
-        term (str): The search term passed to the API.
-        location (str): The search location passed to the API.
+        Args:
+            term (str): The search term passed to the API.
+            location (str): The search location passed to the API.
 
-    Returns:
-        dict: The JSON response from the request.
-    """
-    
-    if ll:
-        url_params = {
-            'term': term.replace(' ', '+'),
-            'll': ','.join(ll),
-            'limit': limit,
-            'radius_filter': radius
-        }
-    else:
-        url_params = {
-            'term': term.replace(' ', '+'),
-            'location': location.replace(' ', '+'),
-            'limit': limit,
-            'radius_filter': radius
-        }
-    return request(API_HOST, SEARCH_PATH, url_params=url_params)
+        Returns:
+            dict: The JSON response from the request.
+        """
+        
+        if ll:
+            url_params = {
+                'term': term.replace(' ', '+'),
+                'll': ','.join(ll),
+                'limit': limit,
+                'radius_filter': radius
+            }
+        else:
+            url_params = {
+                'term': term.replace(' ', '+'),
+                'location': location.replace(' ', '+'),
+                'limit': limit,
+                'radius_filter': radius
+            }
+        return self.request(API_HOST, SEARCH_PATH, url_params=url_params)
 
-def get_business(business_id):
-    """Query the Business API by a business ID.
+    def get_business(self, business_id):
+        """Query the Business API by a business ID.
 
-    Args:
-        business_id (str): The ID of the business to query.
+        Args:
+            business_id (str): The ID of the business to query.
 
-    Returns:
-        dict: The JSON response from the request.
-    """
-    business_path = BUSINESS_PATH + business_id
+        Returns:
+            dict: The JSON response from the request.
+        """
+        business_path = BUSINESS_PATH + business_id
 
-    return request(API_HOST, business_path)
+        return self.request(API_HOST, business_path)
 
-def query_api(ll,
-              term = DEFAULT_TERM, 
-              location = DEFAULT_LOCATION,
-              limit = DEFAULT_SEARCH_LIMIT, 
-              radius = DEFAULT_RADIUS):
-    """Queries the API by the input values from the user.
+    def query_api(self, 
+                  ll,
+                  term = DEFAULT_TERM, 
+                  location = DEFAULT_LOCATION,
+                  limit = DEFAULT_SEARCH_LIMIT, 
+                  radius = DEFAULT_RADIUS):
+        """Queries the API by the input values from the user.
 
-    Args:
-        term (str): The search term to query.
-        location (str): The location of the business to query.
-    """
-    response = search(term, location, ll, limit, radius)
+        Args:
+            term (str): The search term to query.
+            location (str): The location of the business to query.
+        """
+        response = self.search(term, location, ll, limit, radius)
+        businesses = response.get('businesses')
+        return businesses
 
-    businesses = response.get('businesses')
+        # if not businesses:
+        #     print u'No businesses for {0} in {1} found.'.format(term, location)
+        #     return
 
-    # if not businesses:
-    #     print u'No businesses for {0} in {1} found.'.format(term, location)
-    #     return
+        # business_id = businesses[0]['id']
 
-    # business_id = businesses[0]['id']
+        # print u'{0} businesses found, querying business info for the top result "{1}" ...'.format(
+        #     len(businesses),
+        #     business_id
+        # )
 
-    # print u'{0} businesses found, querying business info for the top result "{1}" ...'.format(
-    #     len(businesses),
-    #     business_id
-    # )
+        # response = get_business(business_id)
 
-    # response = get_business(business_id)
-
-    # print u'Result for business "{0}" found:'.format(business_id)
-    # pprint.pprint(response, indent=2)
-    return businesses
+        # print u'Result for business "{0}" found:'.format(business_id)
+        # pprint.pprint(response, indent=2)
 
 
 
@@ -170,7 +171,7 @@ def main():
     input_values = parser.parse_args()
 
     try:
-        businesses = query_api(term=input_values.term, 
+        businesses = YelpQuery().query_api(term=input_values.term, 
                   location=input_values.location, 
                   ll=input_values.ll,
                   limit=input_values.limit, 
